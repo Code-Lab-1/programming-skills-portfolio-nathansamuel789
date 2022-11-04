23aa065d0e0fd2f8d09136fe149121fa66b35e6f
Nathan Samuel <nathansamuel1816@gmail.com>
2022-11-05 01:18:04 +0400

    ExerciseS

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,22 @@
+glossary = {
+    'string': 'A series of characters.',
+    'if..else': 'Python supports the usual logical conditions from mathematics',
+    'list': 'A collection of items in a particular order.',
+    'dictionary': "A collection of key-value pairs.",
+    'operators':'Operators are used to perform operations on variables and values.',
+    }
+
+word = 'string'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'if..else'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'list'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'dictionary'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'operators'
+print("\n" + word.title() + ": " + glossary[word])
---
+++ Chapter 5- Dictionaries/Exercises/Exercise 5 .py
@@ -0,0 +1,20 @@
+Milo= {
+	'species': 'Dog',
+	'color': 'brown',
+	'owner': 'Nathan',
+}
+Ronald= {
+	'species': 'cat',
+	'color': 'orange',
+	'owner': 'Raven',
+}
+Elisha= {
+	'species': 'dog',
+	'color': 'black',
+	'owner': 'Arnold',
+}
+
+pets = [Milo, Ronald, Elisha]
+
+for pet in pets:
+	print(pet)
