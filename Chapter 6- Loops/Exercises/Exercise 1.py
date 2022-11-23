b5d70f07b6f39386e5ab87fd41a766f4a08c95b0
Nathan Samuel <nathansamuel1816@gmail.com>
2022-11-23 14:35:29 +0400

    Update Exercise 1.py

--- Chapter 6- Loops/Exercises/Exercise 1.py
+++ Chapter 6- Loops/Exercises/Exercise 1.py
@@ -1,9 +1,9 @@
-prompt = "\nWhat toppings would you like to add on your pizza?"
-prompt += "\nEnter 'quit' when you are done:"
+prompt = "\nWhat toppings would you want to add on your pizza?"
+prompt += "\nEnter 'finshed' when you are done:"
 
 while True:
     topping = input(prompt)
     if topping != 'quit':
-        print("  I'll add " + topping + " to your pizza.")
+        print("  I will add " + topping + " to your pizza.")
     else:
         break
