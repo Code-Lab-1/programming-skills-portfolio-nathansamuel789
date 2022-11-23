e7419eb822984a37ff6f9ff383a04daa261b850a
Nathan Samuel <nathansamuel1816@gmail.com>
2022-11-23 15:01:56 +0400

    7

---
+++ Chapter 7- Functions/Exercise 2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    """Display a message about my favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('Goosebumps')
---
+++ Chapter 7- Functions/Exercise 4.py
@@ -0,0 +1,8 @@
+def make_shirt(size='large', message='BANKAIII!!'):
+    """ Display the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt()
+make_shirt( size='medium')
+make_shirt('small','Tensa Zangetsu')
