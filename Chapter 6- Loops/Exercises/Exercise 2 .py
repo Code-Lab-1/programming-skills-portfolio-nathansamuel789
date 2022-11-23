658368cfe7fa9cf549852c24b363d45099427502
Nathan Samuel <nathansamuel1816@gmail.com>
2022-11-23 14:40:10 +0400

    6

--- Chapter 6- Loops/Exercises/Exercise 2 .py
+++ Chapter 6- Loops/Exercises/Exercise 2 .py
@@ -1,15 +1,15 @@
 question= "How old are you?"
-question += "\nEnter 'quit' when you are finished. "
+question += "\nEnter 'done' when you are finished. "
 
 while True:
     age = input(question)
-    if age == 'quit':
+    if age == 'done':
         break
     age = int(age)
 
     if age < 3:
-        print("  You get in free!")
+        print("You get a free ticket")
     elif age < 13:
-        print("  Your ticket will be $10.")
+        print("  Your ticket will be 36 dhs.")
     else:
-        print("  Your ticket will be $15.")
+        print("  Your ticket will be 45 dhs.")
